602b9781d15cf9fde75859661f973a2cf891066e
AleksanderSuchodolski <129153686+AleksanderSuchodolski@users.noreply.github.com>
2024-02-06 02:28:06 +0100

    04-auth

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 import contactsController from '../../controllers/contacts-controller.js';
 
-import { isEmptyBody, isEmptyBodyFavorite, isValidId } from "../../midlewares/index.js";
+import { isEmptyBody, isEmptyBodyFavorite, isValidId, authenticate } from "../../midlewares/index.js";
 
 import { validateBody } from '../../decorators/index.js';
 
@@ -11,17 +11,17 @@ import { contactPostSchema, contactPutSchema, contactPatchSchema } from '../../m
 
 const contactsRouter = express.Router()
 
-contactsRouter.get('/', contactsController.getAll);
+contactsRouter.get('/', authenticate, contactsController.getAll);
 
-contactsRouter.get('/:contactId', isValidId, contactsController.getById);
+contactsRouter.get('/:contactId', authenticate, isValidId, contactsController.getById);
 
-contactsRouter.post('/', isEmptyBody, validateBody(contactPostSchema), contactsController.add);
+contactsRouter.post('/', authenticate, isEmptyBody, validateBody(contactPostSchema), contactsController.add);
 
-contactsRouter.put('/:contactId', isValidId, isEmptyBody, validateBody(contactPutSchema), contactsController.updateById);
+contactsRouter.put('/:contactId', authenticate, isValidId, isEmptyBody, validateBody(contactPutSchema), contactsController.updateById);
 
-contactsRouter.patch('/:contactId/favorite', isValidId, isEmptyBodyFavorite, validateBody(contactPatchSchema), contactsController.updateStatusContact);
+contactsRouter.patch('/:contactId/favorite', authenticate, isValidId, isEmptyBodyFavorite, validateBody(contactPatchSchema), contactsController.updateStatusContact);
 
-contactsRouter.delete('/:contactId', isValidId, contactsController.deleteById);
+contactsRouter.delete('/:contactId', authenticate, isValidId, contactsController.deleteById);
 
 
 
